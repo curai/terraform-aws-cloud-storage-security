8c3df0890a40aa0ba16a925775dca98919a72521
Kara <kara@curai.com>
2026-03-18 15:57:33 -0700

    Specify version withotu constraints

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source      = "hashicorp/aws"
-      version     = "6.37.0"
-      constraints = ">= 4.0.0, ~> 6.37.0"
+      source  = "hashicorp/aws"
+      version = "~> 6.37.0"
     }
     awscc = {
       source  = "hashicorp/awscc"
